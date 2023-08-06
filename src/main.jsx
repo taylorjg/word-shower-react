f4d5069401bb1da7aa7be8a9d47d3a5bae23cd29
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-08-06 11:52:25 +0100

    Add global styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import { App } from "./App.jsx";
+import { GlobalStyles } from "./Global.styles.js";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
