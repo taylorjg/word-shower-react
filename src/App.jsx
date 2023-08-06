2476366566ed2dfb6c12678d8b4c8c24c1a74519
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-08-06 11:44:10 +0100

    Add fontawesome

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleQuestion, faGear, faClose } from "@fortawesome/free-solid-svg-icons";
+
 export const App = () => {
-  return <div>TODO</div>;
+  return (
+    <div>
+      <div>TODO</div>
+      <FontAwesomeIcon icon={faCircleQuestion} />
+      <FontAwesomeIcon icon={faGear} />
+      <FontAwesomeIcon icon={faClose} />
+    </div>
+  );
 };
