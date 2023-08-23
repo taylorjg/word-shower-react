677c738a37320d4efdeddb5b8d2bb9d132016527
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-08-23 08:19:30 +0100

    Remove react-spring - just use CSS animation

--- src/components/AnimatedLetter.jsx
+++ src/components/AnimatedLetter.jsx
@@ -1,6 +1,5 @@
 import { useRef } from "react";
 import PropTypes from "prop-types";
-import { useSpring } from "@react-spring/web";
 
 import { Letter } from "./Letter";
 import { StyledAnimatedLetter } from "./AnimatedLetter.styles";
@@ -10,15 +9,13 @@ export const AnimatedLetter = ({ letter }) => {
   const size = "large";
   const letterSize = getLetterSize("large");
   const leftRef = useRef(`calc((100% - ${letterSize}) * ${Math.random()})`);
-
-  const springs = useSpring({
-    from: { top: "0%" },
-    to: { top: "100%" },
-    config: { duration: 4000 },
-  });
+  const letterFallSpeed = 4000;
 
   return (
-    <StyledAnimatedLetter style={{ left: leftRef.current, ...springs }}>
+    <StyledAnimatedLetter
+      $left={leftRef.current}
+      $letterFallSpeed={letterFallSpeed}
+    >
       <Letter letter={letter} size={size} />
     </StyledAnimatedLetter>
   );
--- src/components/AnimatedLetter.styles.js
+++ src/components/AnimatedLetter.styles.js
@@ -1,8 +1,21 @@
-import { styled } from "styled-components";
-import { animated } from "@react-spring/web";
+import { styled, keyframes } from "styled-components";
 
-export const StyledAnimatedLetter = styled(animated.div)`
+const fall = keyframes`
+  from {
+    top: 0;
+  }
+  to {
+    top: 100%;
+  }
+`;
+
+export const StyledAnimatedLetter = styled.div`
   width: 100%;
   height: 100%;
   position: absolute;
+  left: ${(props) => props.$left};
+  animation-duration: ${(props) => `${props.$letterFallSpeed}ms`};
+  animation-name: ${fall};
+  animation-fill-mode: forwards;
+  animation-timing-function: linear;
 `;
