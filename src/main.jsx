66201602ac70d1da1faf22972db329dcc7cc3700
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-08-24 19:55:38 +0100

    Add Google Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -12,3 +12,19 @@ ReactDOM.createRoot(root).render(
     <App />
   </React.StrictMode>
 );
+
+if (window.location.hostname === "taylorjg.github.io") {
+  const script1 = document.createElement("script");
+  script1.async = true;
+  script1.src = "https://www.googletagmanager.com/gtag/js?id=G-LLWBCGS18Y";
+  document.head.appendChild(script1);
+
+  const script2 = document.createElement("script");
+  script2.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-LLWBCGS18Y');
+  `;
+  document.head.appendChild(script2);
+}
