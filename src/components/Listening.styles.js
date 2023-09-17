eb2da2eef6e097a2fd22742ff5e26eccc3062929
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-09-17 22:04:26 +0100

    Add pulse animation to red listening dot in header

--- src/components/Listening.styles.js
+++ src/components/Listening.styles.js
@@ -1,4 +1,4 @@
-import { styled } from "styled-components";
+import { styled, keyframes } from "styled-components";
 
 export const StyledListening = styled.div`
   display: flex;
@@ -20,9 +20,28 @@ export const StyledLower = styled.div`
   font-style: italic;
 `;
 
+const pulse = keyframes`
+  0% {
+    scale: 1.0;
+    opacity: 0.75;
+  }
+  50% {
+    scale: 1.25;
+    opacity: 1.0;
+  }
+  100% {
+    scale: 1.0;
+    opacity: 0.75;
+  }
+`;
+
 export const StyledDot = styled.span`
   color: red;
   &:after {
     content: "\u2b24";
   }
+  animation-name: ${pulse};
+  animation-duration: 1500ms;
+  animation-timing-function: ease-in-out;
+  animation-iteration-count: infinite;
 `;
